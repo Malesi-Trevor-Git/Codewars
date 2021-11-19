7ca26afea4740a514ce0ae1149632562b01bde3a
Malesi-Trevor-Git <trevormalesi4@gmail.com>
2021-11-19 13:52:01 +0300

    Another commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println(GetFibonacci.getFibonacci(100));
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("c");
+        list.add("a");
+        list.add("z");
+        list.add("q");
+        Iterator<String> it=list.iterator();
+        while (it.hasNext()){
+            String a=it.next();
+            System.out.println(a);
+        }
     }
 }
