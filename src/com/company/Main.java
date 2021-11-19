f80b85681f68d02a9008229edf190a5764953170
Malesi-Trevor-Git <trevormalesi4@gmail.com>
2021-11-19 14:05:51 +0300

    Bogus commit...to be changed later

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,18 +6,7 @@ import java.util.Collections;
 import java.util.Iterator;
 
 public class Main{
-    public static void main(String[] args) {
-        System.out.println(Main.getHigher(21));
-    }
-    public static int getHigher(int n){
-        int next=n+100;
-        int result=0;
-        for (int i=n;i<=n+100000;i++){
-            if (i%5==0){
-                result=i;
-                break;
-            }
-        }
-        return result;
+    public static void main(String[] args){
+
     }
 }
